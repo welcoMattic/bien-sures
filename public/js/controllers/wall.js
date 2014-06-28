c40088331ebc457745a66d13bea3f2ac4d633d3e
Aurélien DIGOUT <aurelien.digout@gmail.com>
2014-06-28 17:40:52 +0200

    zogzog2

--- public/js/controllers/wall.js
+++ public/js/controllers/wall.js
@@ -20,6 +20,7 @@ BSApp.controller('WallCtrl', function($rootScope, $scope, Typologies, Reply) {
   $scope.$wall = $('#wall');
 
   $scope.$addReply = $('#addReply');
+  $scope.$addReplySelect = $('#addReply ul');
   $scope.$reply = $('#reply');
   $scope.$filters = $('#filters');
 
@@ -93,7 +94,8 @@ BSApp.controller('WallCtrl', function($rootScope, $scope, Typologies, Reply) {
   }
 
   $scope.clearAddReply = function() {
-    $scope.$addReply.find('select option[value=0]').attr('selected','selected');
+    $scope.$addReply.find( '.select .typeTxt' ).html( "Type d'agression" );
+    $scope.$addReply.find( '.select' ).attr( 'data-value', 'null' );
     $scope.$addReply.find('textarea').val('');
     $scope.replyLength = 140;
   }
@@ -185,6 +187,10 @@ BSApp.controller('WallCtrl', function($rootScope, $scope, Typologies, Reply) {
     $scope.$reply.hide();
   }
 
+  $scope.addReplySelect = function() {
+
+  }
+
   $scope.liveLength = function() {
     $scope.replyLength = 140 - ($scope.$addReply.find('textarea').val().length);
     // if( $scope.replieLength == 0 )
@@ -194,11 +200,11 @@ BSApp.controller('WallCtrl', function($rootScope, $scope, Typologies, Reply) {
   }
 
   $scope.addReply = function() {
-    var typeId = $scope.$addReply.find('#type').val(),
+    var typeId = $scope.$addReply.find( '.select' ).attr( 'data-value' ),
       reply = $scope.$addReply.find('textarea').val(),
       validToInsert = true;
 
-    if (!typeId || !reply || typeId == "0") {
+    if (!typeId || !reply || typeId == "null") {
       validToInsert = false;
       return false;
     }
@@ -220,6 +226,23 @@ BSApp.controller('WallCtrl', function($rootScope, $scope, Typologies, Reply) {
     });
 
     return false;
+  }
+
+  $scope.showSelect = function() {
+    $( '.btnList' ).addClass( 'open' );
+    $scope.$addReplySelect.show();
+  }
+
+  $scope.hideSelect = function() {
+    $( '.btnList' ).removeClass( 'open' );
+    $scope.$addReplySelect.hide();
+  }
+
+  $scope.seltectRemplace = function( $event ) {
+    var $target = $($event.target);
+    $scope.$addReply.find( '.select .typeTxt' ).html( $target.html() );
+    $scope.$addReply.find( '.select' ).attr( 'data-value', $target.attr('data-value') );
+    $scope.hideSelect(); 
   }
 
   twttr.ready(function(twttr) {       
